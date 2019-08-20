439bbe38e17dca12f1f8bbfbd78ce915f67ed2ef
Maria Rizo <mrizo036@west-mec.org>
2019-08-19 18:23:14 -0700

    Changes Made to app.js

---
+++ sub-unit-1/02_let_const/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var a = "Test1";
+var b = "Test2";
+
+console.log(`Logged: ${a}`)
+
+ const testVar = (params) => {
+     let a = 10;
+     console.log(`Function Logged: ${a}`);
+
+    if (true)  {
+    var a = 50;
+    console.log(`Block Logged: ${a}`);
+    }
+    console.log("here: " + a);
+}
+
+const addSelf = a => a+=a;
+
+
+testVar();
+console.log(`END Logged: ${a}`)
